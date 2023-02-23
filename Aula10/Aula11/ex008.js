649d0b1fe909685ca774acb6237fe3469cbad5a8
luizViniciusZeferino <luizvinicius1530@gmail.com>
2023-02-22 22:13:40 -0300

    Módulo D

---
+++ Aula10/Aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 60.1
+console.log(`A velocidade do carro é ${velocidade}Km/h`)
+if (velocidade > 60) { // SE a VELOCIDDADE for MAIOR  que 60 MULTADO. CLASSIFICADO COMO CONDIÇÃO SIMPLES.
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`) // Esta dentro do bloco. 
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
---
+++ Aula10/Aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil'){ // Se você nasceu no Brasil vocé é Brasileiro .
+    console.log('Você é Brasileiro')
+}else {// Se não você é estrangeiro.
+    console.log ('Você é Estrangeiro')
+}
