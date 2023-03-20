733d5914b4bc762fda363170ec6dfc172f452b08
luizViniciusZeferino <luizvinicius1530@gmail.com>
2023-03-20 20:57:48 -0300

    final do ex14

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -4,18 +4,29 @@ function Contar() {
     var passo = document.getElementById('txtp');
     var resposta = document.getElementById('res');
 
-    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('Impossível contar');
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) { // lenght significa quantidade de caracteres. Se o valor das variaveis forem 0 = alert
+        window.alert('{[ERRO] Faltam Dados!');
+        res.innerHTML = 'Impossível contar!'
     } else {
-        resposta.innerHTML = "Contando...";
-        var i = Number(inicio.value);
+        resposta.innerHTML = "Contando:  <br>  "; // br para fazer a quebra de linha em html. 
+
+        var i = Number(inicio.value); // fazendo a mudança para numero 
         var f = Number(fim.value);
         var p = Number(passo.value);
         var res = resposta;
-
-        for (var contador = i; contador <= f; contador += p) { // parei aqui 
-            res.innerHTML += `${contador} `;
+        if (p <= 0) {
+            window.alert ('Passo inválido, considerando PASSO 1')
+            p = 1 
         }
+        if ( i < f ) {
+            for (var contador = i; contador <= f; contador += p) { // crescente
+                res.innerHTML += `${contador} \u{1F449} `;
+             }
+        }else {
+            for (var contador = i; contador >= f; contador -= p ) // regressiva 
+            res.innerHTML += `${contador} \u{1F449}`
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
 
@@ -24,5 +35,5 @@ function Contar() {
 
 
 
-Reg
+
     
